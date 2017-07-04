73a32c132d4ff7c8d17fc0aa5fb24f07f3cfd65d
Peter Georgiev <peter_85@abv.bg>
2017-07-04 15:50:13 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos }); // more flexible way - give us chance to add more keys afterwards
+    })
+}, (err) => {
+    res.status(400).send(e);
+})
+
 app.listen(3010, () => {
     console.log('Started on port 3010');
 });
