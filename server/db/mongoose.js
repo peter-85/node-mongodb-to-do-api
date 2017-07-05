d90a22a0c3a1747720a2912cb7ae23918861d772
Peter Georgiev <peter_85@abv.bg>
2017-07-05 18:56:43 +0300

    changes for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
     mlab: 'mongodb://<peter_85>:<fce13382>@ds149122.mlab.com:49122/to-do-app-udemy'
 };
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(db.mlab || db.localhost);
 
 module.exports = { mongoose };
